e629360774f6e7051520d1e8e825c031e0db70f6
askaik <76906248+askaik@users.noreply.github.com>
2026-02-23 18:13:37 +0300

    Add Frappe app boilerplate files

---
+++ README.md
@@ -0,0 +1,16 @@
+# Order Picking
+
+Order Picking App for ERPNext to ensure 100% accurate deliveries
+
+## Installation
+
+You can install this app using the `bench` CLI:
+
+```bash
+bench get-app https://github.com/your-username/order_picking
+bench --site your-site-name install-app order_picking
+```
+
+## License
+
+MIT
---
+++ order_picking/hooks.py
@@ -0,0 +1,33 @@
+app_name = "order_picking"
+app_title = "Order Picking"
+app_publisher = "Your Company"
+app_description = "Order Picking App for ERPNext to ensure 100% accurate deliveries"
+app_email = "hello@example.com"
+app_license = "mit"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/order_picking/css/order_picking.css"
+# app_include_js = "/assets/order_picking/js/order_picking.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/order_picking/css/order_picking.css"
+# web_include_js = "/assets/order_picking/js/order_picking.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "order_picking/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
